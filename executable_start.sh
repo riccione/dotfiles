fde4b7ab9c7e9770a15bb064da7af53a22d9c3da
riccione <riccione@protonmail.com>
2025-08-10 10:34:41 -0400

    Add an env var XDG_DATA_DIRS to start.sh Comment out include from sway config
    Present of the include overrided my sway config!

--- executable_start.sh
+++ executable_start.sh
@@ -10,10 +10,11 @@ export GDK_DPI_SCALE=1.5
 export QT_AUTO_SCREEN_SCALE_FACTOR=1.5
 export QT_SCALE_FACTOR=1.5
 
-# export MOZ_ENABLE_WAYLAND=1
+export XDG_DATA_DIRS=/usr/local/share:/usr/share
 
 # GNOME keyring
 eval $(/usr/bin/gnome-keyring-daemon --start --components=secrets,pkcs11,ssh)
 export SSH_AUTH_SOCK
 
+# for logging add after sway '-d 2> ~/sway.log'
 exec dbus-run-session sway
