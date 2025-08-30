2eb1c8ce1cca8b61e64e44a1205d48ed32e2485f
riccione <riccione@protonmail.com>
2025-08-30 10:19:04 -0400

    Update llama.sh script

--- dot_scripts/executable_llama.sh
+++ dot_scripts/executable_llama.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-LLAMA_DIR=~/bin/llama.cpp
+LLAMA_DIR=~/bin/llama.cpp/build/bin
 LD_LIBRARY_PATH=$LLAMA_DIR $LLAMA_DIR/llama-server "$@"
+export LD_LIBRARY_PATH="$LLAMA_DIR:$LD_LIBRARY_PATH"
+"$LLAMA_DIR/llama-server" -m ~/Documents/llm/models/Qwen2.5.1-Coder-7B-Instruct-Q8_0.gguf
